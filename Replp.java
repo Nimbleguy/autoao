05932e2462f48bf4fcea5a586557191ca87460d4
Nimbleguy <nomblest@gmail.com>
2016-11-12 23:35:16 -0500

    Initial commit.

---
+++ Replp.java
@@ -0,0 +1,37 @@
+import java.util.regex.Pattern;
+
+public class Replp {
+  static boolean pchum = true;
+  public static void main(String[] args)
+  {
+    Pattern others = Pattern.compile("^.+?:\\ ");
+    Pattern irilib = Pattern.compile("^Erelye:\\ ");
+    Pattern irilia = irilib;
+
+    if(pchum){
+      others = Pattern.compile("^.+?:\\ ");
+      irilia = Pattern.compile("^.AO:\\ ");
+      irilib = Pattern.compile("^.AT:\\ ");
+    }
+
+    String out = "";
+    for(String i : args){
+      i = i.replaceAll("\n", "").replaceAll("\r", "");
+      i = i.replaceAll("→", " ");
+      if(irilib.matcher(i).find()){
+        i = irilib.matcher(i).replaceAll("[OUTPUT: ");
+        i = i + "]";
+      }
+      else if(irilia.matcher(i).find()){
+        i = irilia.matcher(i).replaceAll("[OUTPUT: ");
+        i = i + "]";
+      }
+      else{
+        i = others.matcher(i).replaceAll("");
+      }
+      i = i.replaceAll("\\[\\[\\*\\*", "\\[").replaceAll("\\*\\*\\]\\]", "\\]").replaceAll("\\[\\[\\*", "\\[").replaceAll("\\*\\]\\]", "\\]");
+      out = out + "\n" + i;
+    }
+    System.out.println(out);
+  }
+}
